4f87b2631b7a7318f663d6283c356d3070163ab7
Rosyieed <rosyid123.ren@gmail.com>
2024-04-19 08:40:28 +0700

    Feat: CRUD Cashier

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CashierController;
+use App\Http\Controllers\ProfileController;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,10 +23,21 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
+Route::middleware('auth', 'super_admin')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::middleware(['cashier'])->group(function () {
+        Route::get('/cashier', [CashierController::class, 'index'])->name('cashier.index');
+        Route::get('/cashier/create', [CashierController::class, 'create'])->name('cashier.create');
+        Route::post('/cashier', [CashierController::class, 'store'])->name('cashier.store');
+        // Route::get('/cashier/{id}', [CashierController::class, 'show'])->name('cashier.show');
+        Route::get('/cashier/{id}/edit', [CashierController::class, 'edit'])->name('cashier.edit');
+        Route::patch('/cashier/{id}', [CashierController::class, 'update'])->name('cashier.update');
+        Route::delete('/cashier/{id}', [CashierController::class, 'destroy'])->name('cashier.destroy');
+    });
 });
 
-require __DIR__.'/auth.php';
+
+require __DIR__ . '/auth.php';
